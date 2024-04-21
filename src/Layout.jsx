6ca6e73796017c1cc2570fade60116a888b8b935
TejaSaii <tejas7833@gmail.com>
2024-04-21 19:44:01 +0530

    Initial commit

---
+++ src/Layout.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Outlet from "react-router-dom";
+
+const Layout = () => {
+  <>
+  <Header />
+  <Outlet />
+  <Footer />
+  </>
+}
+
+export default Layout;
